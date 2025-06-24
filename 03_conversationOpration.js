d277293ebb805417ae7fa340e9b3f07d0486be41
ashish kumar rana <ra6009798@gmail.com>
2025-06-25 03:36:15 +0530

    Add files via upload
    done

---
+++ 03_conversationOpration.js
@@ -0,0 +1,30 @@
+let  score = "33abc"
+
+console.log(typeof(score));
+console.log(typeof(score));
+
+let valueInNmber = Number(score)
+
+console.log(typeof valueInNmber);
+console.log(valueInNmber);
+
+
+// "33"  33
+
+// "33abx"  = NaN
+
+// true = 1
+
+islogin = 1;
+
+ let isbolleanislogin = Boolean(islogin)
+
+ console.log(isbolleanislogin);
+ 
+
+ let somenum = 23;
+
+ let somestr = String("ashish")
+
+ console.log(somenum);
+ 
