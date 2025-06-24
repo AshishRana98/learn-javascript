e3d9dedf20ea5fab5bb915f8a116c0ea8555be2b
ashish kumar rana <ra6009798@gmail.com>
2025-06-25 02:58:38 +0530

    Add files via upload
    done

---
+++ 01_varible.js
@@ -0,0 +1,23 @@
+let a = 19
+let b= 20
+
+let sum = a+b
+console.log(sum);
+
+
+const account_id = 456787657465
+let email = 'fhdjs@gmail.com'
+var acc_password = "456789"
+acc_city = "ranchi"
+
+
+
+// account_id = 4588
+email = 'ashish@gmail.com'
+acc_password = "4569"
+acc_city = "banglore"
+
+console.table([acc_password,email,account_id,acc_city])
+
+
+
---
+++ 02_data_type.js
@@ -0,0 +1,26 @@
+"use strick";// " all code newer version treat"
+  //alert (3+3)  // we are using node js , not browser
+
+  console.log(3+3); 
+   console.log("hi how are you");
+  
+  
+let name = "ashish"  //string
+let age = 23 // int
+let istrue = false//bollean
+let set = null // null
+let state;  //undefined
+
+
+//number  2 to pow  53
+
+//bigint
+//string
+//bolean true false 
+//null
+//undefine
+//symbole
+
+// Object
+
+console.log(typeof(null));
